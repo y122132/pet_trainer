517318d97d0ca053bb28142d79060833c0f41ddd
Yang <y122132@naver.com>
2026-01-12 09:44:32 +0900

    tflite버전 변경

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -34,7 +34,13 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            // [Fix] Add Proguard Rules for TFLite
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
         }
     }
 }
